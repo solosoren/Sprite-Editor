60529c0504679dcaddaf4975ad6e3c3c720168a5
studydude <studydude@protonmail.com>
2018-11-04 22:09:12 -0700

    init tools.h

--- SpriteEditor/tools.h
+++ SpriteEditor/tools.h
@@ -1,11 +1,38 @@
 #ifndef TOOLS_H
 #define TOOLS_H
 
+#include <QObject>
+#include <QColor>
+#include <QPainter>
+#include <vector>
 
-class Tools
+int selectedTool = 0;
+
+class Tools : public QObject
 {
-public:
-    Tools();
+    Q_OBJECT
+
+    public:
+        Tools();
+
+        std::vector<QPointF> startPos(QPointF StartPos);
+        std::vector<QObject> endPos(QPointF endPos);
+
+
+    private:
+        /* define tools */
+        const int PEN_TOOL = 0;
+        const int ERASER = 1;
+        const int LINE_TOOL = 2;
+        const int FILL_TOOL = 3;
+        const int BRUSH_TOOL = 4;
+
+        QColor currentColor;
+        QPainter doPaint;
+        QImage tmpImage;
+        QImage currentImage;
+
+
 };
 
 #endif // TOOLS_H
