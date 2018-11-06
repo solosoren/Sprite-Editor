7fa750cfbe071cee8ad55bb769d320777f8b1c4a
Soren Nelson <sorennelson33@gmail.com>
2018-11-06 14:31:46 -0700

    imagesDidUpdate() in Animation

--- SpriteEditor/spriteanimation.cpp
+++ SpriteEditor/spriteanimation.cpp
@@ -3,7 +3,14 @@
 SpriteAnimation::SpriteAnimation(std::vector<QImage*>& images)
 {
     images = images;
+    imagesDidChange = false;
     frameRate = 0;
+}
+
+void SpriteAnimation::imagesDidUpdate()
+{
+    imagesDidChange = true;
+    animate();
 }
 
 // SetFrameRate Slot
@@ -12,15 +19,15 @@ void SpriteAnimation::animate()
 {
     int frameIndex = 0;
     int frameCount = 0;
-    while (!imagesDidChange && frameRate > 0)
+    while (!imagesDidChange && frameRate > 0 && images->size() > 0)
     {
        QImage  image   = *((*images)[frameIndex]);
        QPixmap pixmap  = convertImageToPixmap(image);
-       QTimer::singleShot(frameCount++ * (1000 / frameRate), this, SLOT(displayFrame(frameIndex)));
+       QTimer::singleShot(frameCount++ * (1000 / frameRate), this, SLOT(displayFrame(pixmap&)));
     }
 }
 
-void SpriteAnimation::displayFrame(int frameIndex)
+void SpriteAnimation::displayFrame(QPixmap* pixmap)
 {
 
 }
