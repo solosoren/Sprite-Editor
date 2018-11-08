9591976cffc3e2a7f5418065253ef6d557157d5a
Soren Nelson <sorennelson33@gmail.com>
2018-11-07 17:25:55 -0700

    Update animation loop

--- SpriteEditor/spriteanimation.cpp
+++ SpriteEditor/spriteanimation.cpp
@@ -1,14 +1,14 @@
 #include "spriteanimation.h"
 
-SpriteAnimation::SpriteAnimation(std::vector<QImage*>& images)
+SpriteAnimation::SpriteAnimation()
 {
-    images = images;
     imagesDidChange = false;
     frameRate = 0;
 }
 
-void SpriteAnimation::imagesDidUpdate()
+void SpriteAnimation::setImages(std::vector<QImage*>* images)
 {
+    images = images;
     imagesDidChange = true;
     animate();
 }
@@ -19,15 +19,18 @@ void SpriteAnimation::animate()
 {
     int frameIndex = 0;
     int frameCount = 0;
-    while (!imagesDidChange && frameRate > 0 && images->size() > 0)
+    while (true)
     {
-       QImage  image   = *((*images)[frameIndex]);
-       QPixmap pixmap  = convertImageToPixmap(image);
-       QTimer::singleShot(frameCount++ * (1000 / frameRate), this, SLOT(displayFrame(pixmap&)));
+        if (frameRate > 0 && images->size() > 0)
+        {
+            QImage  image   = *((*images)[frameIndex++ % images->size()]);
+            QPixmap pixmap  = convertImageToPixmap(image);
+            QTimer::singleShot(frameCount++ * (1000 / frameRate), this, SLOT(displayFrame(pixmap)));
+        }
     }
 }
 
-void SpriteAnimation::displayFrame(QPixmap* pixmap)
+void SpriteAnimation::displayFrame(QPixmap pixmap)
 {
-
+    this->addPixmap(pixmap);
 }
